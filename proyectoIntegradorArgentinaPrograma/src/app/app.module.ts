3f61bfb623fcada8fb25784aa6366dd5d9bbde06
AYUSEF7 <110066500+AYUSEF7@users.noreply.github.com>
2023-03-17 23:49:25 -0300

    v8

--- proyectoIntegradorArgentinaPrograma/src/app/app.module.ts
+++ proyectoIntegradorArgentinaPrograma/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EducComponent } from './components/educ/educ.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HySskillsComponent } from './components/hy-sskills/hy-sskills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     ExpComponent,
     EducComponent,
     HySskillsComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
